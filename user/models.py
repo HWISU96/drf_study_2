8d6b6bdb64880a4f3ce0c050c1d6fd501a18cb72
HWISU96 <soshyzx123@naver.com>
2022-06-17 14:06:24 +0900

    3. user/models.py에 사용자의 상세 정보를 저장할 수 있는 `UserProfile` 이라는 모델을 생성해주세요

--- user/models.py
+++ user/models.py
@@ -18,3 +18,30 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'username'
+
+# 취미 : 운동
+
+
+class Hobby(models.Model):
+    name = models.CharField("취미 이름", max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+# user detail info table
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, verbose_name="유저", on_delete=models.CASCADE)
+    introduction = models.TextField("자기소개", null=True, blank=True)
+    birthday = models.DateField("생일")
+    age = models.IntegerField("나이")
+    hobby = models.ManyToManyField(Hobby, verbose_name="취미")
+
+    def __str__(self):
+        return f"{self.user.username} 님의 프로필입니다."
+
+# user - user detail : 1:1
+# 한 유저가 두 프로필을 가질 수는 없음
+
+# 쿼리를 날려서 crud를 한다
