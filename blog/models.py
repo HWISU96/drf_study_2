230cbc56647c56b04fe905b6265ebf2054839144
HWISU96 <soshyzx123@naver.com>
2022-06-17 14:14:44 +0900

    5. blog/models.py에 <카테고리 이름, 설명>이 들어갈 수 있는 `Category`라는 모델을 만들어보세요.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField("이름", max_length=50)
+    description = models.TextField("설명")
+
+    def __str__(self):
+        return self.name
