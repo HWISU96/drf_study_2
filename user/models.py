bfb7fba118b9d7ee58c6b8efd04d9c2d05270b59
HWISU96 <soshyzx123@naver.com>
2022-06-17 13:56:11 +0900

    2. user/models.py에 `Custom user model`을 생성한 후 django에서 user table을 생성 한 모델로 사용할 수 있도록 설정해주세요

--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
+
+# custom user model
+
+
+class User(AbstractBaseUser):
+    username = models.CharField("사용자 계정", max_length=50, unique=True)
+    password = models.CharField("비밀번호", max_length=128)
+    email = models.EmailField("이메일 주소", max_length=100)
+    fullname = models.CharField("이름", max_length=20)
+    join_date = models.DateField("가입일", auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+
+    is_admin = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'username'
